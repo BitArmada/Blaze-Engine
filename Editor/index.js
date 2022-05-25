8f4592ac5ed4bfd201ad274aa9462ad0254d07b4
barkai lazimy <BarkaiLazimy@gmail.com>
2022-05-24 22:42:16 -0500

    starting window code

--- Editor/index.js
+++ Editor/index.js
@@ -1,10 +1,12 @@
 import Window from './UI/Window/Window.js';
 import WindowManager from './UI/Window/WindowManager.js';
 
-class TestWindow{
+class TestWindow extends Window{
     
 }
 
 var windowManager = new WindowManager();
 
 var window = new TestWindow();
+
+windowManager.add(window);
